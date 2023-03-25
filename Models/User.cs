11819d92ea41e80c7a95d2a79519f6b455c050cb
Emily421 <emilyroconnell@lewisu.edu>
2023-03-25 15:43:35 -0500

    Added time stuff.

--- Models/User.cs
+++ Models/User.cs
@@ -9,14 +9,28 @@ namespace RazorPagesUser.Models;
 
 public class User
 {
-   [BsonId]
-   [BsonRepresentation(BsonType.ObjectId)]
-   public string? id { get; set;}
-   public string username { get; set;}
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? id { get; set;}
+    public string username { get; set;}
 
-   public string password { get; set;}
-   public string? location { get; set;}
+    public string password { get; set;}
+    public string? location { get; set;}
 
-   public string sessionID { get; set;}
+    public string sessionID { get; set;}
 
+    public TimeSpan? mondayStart { get; set; }
+    public TimeSpan? mondayEnd { get; set; }
+    public TimeSpan? tuesdayStart { get; set; }
+    public TimeSpan? tuesdayEnd { get; set; }
+    public TimeSpan? wednesdayStart { get; set; }
+    public TimeSpan? wednesdayEnd { get; set; }
+    public TimeSpan? thursdayStart { get; set; }
+    public TimeSpan? thursdayEnd { get; set; }
+    public TimeSpan? fridayStart { get; set; }
+    public TimeSpan? fridayEnd { get; set; }
+    public TimeSpan? saturdayStart { get; set; }
+    public TimeSpan? saturdayEnd { get; set; }
+    public TimeSpan? sundayStart { get; set; }
+    public TimeSpan? sundayEnd { get; set; }
 }
--- Pages/UpdateInformation.cshtml.cs
+++ Pages/UpdateInformation.cshtml.cs
@@ -26,7 +26,10 @@ namespace TheCleaningLadies.Pages
             return RedirectToPage("/Seller");
         }
 
-        public void OnPost(string nlocation)
+        public void OnPost(string nlocation, string mondayStart, string mondayEnd, string tuesdayStart, string tuesdayEnd,
+                   string wednesdayStart, string wednesdayEnd, string thursdayStart, string thursdayEnd,
+                   string fridayStart, string fridayEnd, string saturdayStart, string saturdayEnd,
+                   string sundayStart, string sundayEnd)
         {
 
             UserService userService = new UserService();
@@ -41,6 +44,90 @@ namespace TheCleaningLadies.Pages
                     if (nlocation != null && nlocation != "")
                     {
                         theUser.location = nlocation;
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (mondayStart != null && mondayStart != "")
+                    {
+                        theUser.mondayStart = TimeSpan.Parse(mondayStart);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (mondayEnd != null && mondayEnd != "")
+                    {
+                        theUser.mondayEnd = TimeSpan.Parse(mondayEnd);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (tuesdayStart != null && tuesdayStart != "")
+                    {
+                        theUser.tuesdayStart = TimeSpan.Parse(tuesdayStart);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (tuesdayEnd != null && tuesdayEnd != "")
+                    {
+                        theUser.tuesdayEnd = TimeSpan.Parse(tuesdayEnd);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (wednesdayStart != null && wednesdayStart != "")
+                    {
+                        theUser.wednesdayStart = TimeSpan.Parse(wednesdayStart);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (wednesdayEnd != null && wednesdayEnd != "")
+                    {
+                        theUser.wednesdayEnd = TimeSpan.Parse(wednesdayEnd);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (thursdayStart != null && thursdayStart != "")
+                    {
+                        theUser.thursdayStart = TimeSpan.Parse(thursdayStart);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (thursdayEnd != null && thursdayEnd != "")
+                    {
+                        theUser.thursdayEnd = TimeSpan.Parse(thursdayEnd);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (fridayStart != null && fridayStart != "")
+                    {
+                        theUser.fridayStart = TimeSpan.Parse(fridayStart);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (fridayEnd != null && fridayEnd != "")
+                    {
+                        theUser.fridayEnd = TimeSpan.Parse(fridayEnd);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (saturdayStart != null && saturdayStart != "")
+                    {
+                        theUser.saturdayStart = TimeSpan.Parse(saturdayStart);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (saturdayEnd != null && saturdayEnd != "")
+                    {
+                        theUser.saturdayEnd = TimeSpan.Parse(saturdayEnd);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (sundayStart != null && sundayStart != "")
+                    {
+                        theUser.sundayStart = TimeSpan.Parse(sundayStart);
+                        userService.Update(theUser.username, theUser);
+                    }
+
+                    if (sundayEnd != null && sundayEnd != "")
+                    {
+                        theUser.sundayEnd = TimeSpan.Parse(sundayEnd);
                         userService.Update(theUser.username, theUser);
                     }
                 }
