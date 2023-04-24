b663efa8c262dd1e8b8ee45773e3b8f0402f2e23
Emily421 <emilyroconnell@lewisu.edu>
2023-04-23 21:30:14 -0500

    did sprint stuff.

--- Pages/ScheduleAppointment.cshtml.cs
+++ Pages/ScheduleAppointment.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesUser.Services;
+using RazorPagesUser.Models;
 
 namespace RazorPagesScheduleAppointment.Pages
 {
     public class ScheduleAppointmentModel : PageModel
     {
+
+        public User theUser;
         public void OnGet()
         {
+            string id = Request.Query["id"];
+
+            UserService userService = new UserService();
+            theUser = userService.Get(id);
         }
     }
 }
